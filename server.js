a8e88495f4320f763ccb79e864e3e24bff8491f8
Francis Rodrigues <dev@tosystems.net>
2017-10-22 17:02:48 -0200

    Started our server

--- server.js
+++ server.js
@@ -1,132 +1,137 @@
 // BASE SETUP
 // =============================================================================
 
-// call the packages we need
-var express    = require('express');
-var bodyParser = require('body-parser');
-var app        = express();
-var morgan     = require('morgan');
+// Call the packages we need
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const morgan = require('morgan')
 
-// configure app
-app.use(morgan('dev')); // log requests to the console
 
-// configure body parser
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+// Configure our app
+app.use(morgan('dev')) // log requests to the console
 
-var port     = process.env.PORT || 8080; // set our port
+//- Configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
 
-// DATABASE SETUP
-var mongoose   = require('mongoose');
-mongoose.connect('mongodb://node:node@novus.modulusmongo.net:27017/Iganiq8o'); // connect to our database
+const port = process.env.PORT || 8080 // set our port
+
+
+//- DATABASE SETUP
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost:27017/Express-api') // connect to our database
 
 // Handle the connection event
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
+const db = mongoose.connection
+db.on('error', console.error.bind(console, 'connection error:')
 
-db.once('open', function() {
-  console.log("DB connection alive");
-});
+db.once('open', function () {
+    console.log("DB connection alive")
+})
+
 
 // Bear models lives here
-var Bear     = require('./app/models/bear');
+const Bear = require('./app/models/bear')
+
 
 // ROUTES FOR OUR API
 // =============================================================================
 
 // create our router
-var router = express.Router();
+const router = express.Router()
 
 // middleware to use for all requests
-router.use(function(req, res, next) {
-	// do logging
-	console.log('Something is happening.');
-	next();
-});
+router.use(function (req, res, next) {
+    // do logging
+    console.log('Something is happening.'
+    next()
+})
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-router.get('/', function(req, res) {
-	res.json({ message: 'hooray! welcome to our api!' });	
-});
+router.get('/', function (req, res) {
+    res.json({message: 'hooray! welcome to our api!'})
+})
 
 // on routes that end in /bears
 // ----------------------------------------------------
 router.route('/bears')
 
-	// create a bear (accessed at POST http://localhost:8080/bears)
-	.post(function(req, res) {
-		
-		var bear = new Bear();		// create a new instance of the Bear model
-		bear.name = req.body.name;  // set the bears name (comes from the request)
+// create a bear (accessed at POST http://localhost:8080/bears)
+    .post(function (req, res) {
 
-		bear.save(function(err) {
-			if (err)
-				res.send(err);
+        let bear = new Bear()		// create a new instance of the Bear model
+        bear.name = req.body.name  // set the bears name (comes from the request)
 
-			res.json({ message: 'Bear created!' });
-		});
+        bear.save(function (err) {
+            if (err)
+                res.send(err)
 
-		
-	})
+            res.json({message: 'Bear created!'});
+        })
 
-	// get all the bears (accessed at GET http://localhost:8080/api/bears)
-	.get(function(req, res) {
-		Bear.find(function(err, bears) {
-			if (err)
-				res.send(err);
+    })
 
-			res.json(bears);
-		});
-	});
+    // get all the bears (accessed at GET http://localhost:8080/api/bears)
+    .get(function (req, res) {
+        Bear.find(function (err, bears) {
+            if (err)
+                res.send(err)
+
+            res.json(bears)
+        });
+    })
 
 // on routes that end in /bears/:bear_id
 // ----------------------------------------------------
 router.route('/bears/:bear_id')
 
-	// get the bear with that id
-	.get(function(req, res) {
-		Bear.findById(req.params.bear_id, function(err, bear) {
-			if (err)
-				res.send(err);
-			res.json(bear);
-		});
-	})
+// get the bear with that id
+    .get(function (req, res) {
+        Bear.findById(req.params.bear_id, function (err, bear) {
+            if (err)
+                res.send(err)
+            res.json(bear)
+        });
+    })
 
-	// update the bear with this id
-	.put(function(req, res) {
-		Bear.findById(req.params.bear_id, function(err, bear) {
+    // update the bear with this id
+    .put(function (req, res) {
+        Bear.findById(req.params.bear_id, function (err, bear) {
 
-			if (err)
-				res.send(err);
+            if (err)
+                res.send(err)
 
-			bear.name = req.body.name;
-			bear.save(function(err) {
-				if (err)
-					res.send(err);
+            bear.name = req.body.name
+            bear.save(function (err) {
+                if (err)
+                    res.send(err)
 
-				res.json({ message: 'Bear updated!' });
-			});
+                res.json({message: 'Bear updated!'})
+            });
 
-		});
-	})
+        });
+    })
 
-	// delete the bear with this id
-	.delete(function(req, res) {
-		Bear.remove({
-			_id: req.params.bear_id
-		}, function(err, bear) {
-			if (err)
-				res.send(err);
+    // delete the bear with this id
+    .delete(function (req, res) {
+        Bear.remove({
+            _id: req.params.bear_id
+        }, function (err, bear) {
+            if (err)
+                res.send(err)
 
-			res.json({ message: 'Successfully deleted' });
-		});
-	});
+            res.json({message: 'Successfully deleted'})
+        });
+    })
 
 
 // REGISTER OUR ROUTES -------------------------------
-app.use('/api', router);
+app.use('/api', router)
+
 
 // START THE SERVER
 // =============================================================================
-app.listen(port);
-console.log('Magic happens on port ' + port);
+app.listen(port)
+console.log('Magic happens on port ' + port)
