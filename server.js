7e39408bb5760dbf3f3e44894245177af96b8474
Francis Rodrigues <dev@tosystems.net>
2017-10-22 17:27:31 -0200

    Added Route middleware

--- server.js
+++ server.js
@@ -16,6 +16,14 @@ const port = process.env.PORT || 3030 // set our port
 const router = express.Router() // get an instance of the express Router
 
 
+// Middleware to use for all requests.
+router.use(function (req, res, next) {
+    // do logging
+    console.log('Something  is happening.')
+    next() // make sure we go to the next routes and don't stop here.
+})
+
+
 
 //- Database and Bear model.
 var mongoose = require('mongoose')
