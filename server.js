9fd00d0223095b59d1e332f92b20a22c30f77469
Francis Rodrigues <dev@tosystems.net>
2017-10-25 20:20:37 -0200

    Created a Bear POST

--- server.js
+++ server.js
@@ -92,62 +92,8 @@ router.route('/bears')
             if (err)
                 res.send(err)
 
-            res.json({message: 'Bear created!'});
+            res.json({message: 'Bear created!'})
         })
-
-    })
-
-    // get all the bears (accessed at GET http://localhost:8080/api/bears)
-    .get(function (req, res) {
-        Bear.find(function (err, bears) {
-            if (err)
-                res.send(err)
-
-            res.json(bears)
-        });
-    })
-
-// on routes that end in /bears/:bear_id
-// ----------------------------------------------------
-router.route('/bears/:bear_id')
-
-// get the bear with that id
-    .get(function (req, res) {
-        Bear.findById(req.params.bear_id, function (err, bear) {
-            if (err)
-                res.send(err)
-            res.json(bear)
-        });
-    })
-
-    // update the bear with this id
-    .put(function (req, res) {
-        Bear.findById(req.params.bear_id, function (err, bear) {
-
-            if (err)
-                res.send(err)
-
-            bear.name = req.body.name
-            bear.save(function (err) {
-                if (err)
-                    res.send(err)
-
-                res.json({message: 'Bear updated!'})
-            });
-
-        });
-    })
-
-    // delete the bear with this id
-    .delete(function (req, res) {
-        Bear.remove({
-            _id: req.params.bear_id
-        }, function (err, bear) {
-            if (err)
-                res.send(err)
-
-            res.json({message: 'Successfully deleted'})
-        });
     })
 
 
