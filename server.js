80a216d61ecc32067eb793f9b36968f842e5741a
Francis Rodrigues <dev@tosystems.net>
2017-10-26 14:26:25 -0200

    Getted a single Bear GET

--- server.js
+++ server.js
@@ -77,6 +77,9 @@ router.get('/', function (req, res) {
     res.json({message: 'hooray! welcome to our api!'})
 })
 
+//- More routes for our API will happen here.
+//
+
 // on routes that end in /bears
 // ----------------------------------------------------
 router.route('/bears')
@@ -108,7 +111,24 @@ router.route('/bears')
     })
 
 
-// REGISTER OUR ROUTES -------------------------------
+// on routes that end in /bears/:bear_id
+router.route('/bears/:bear_id')
+
+    // Get the bear with that id (accessed at GET http://localhost:8080/api/bears/:bear_id)
+    .get(function (req, res) {
+        Bear.findById(req.params.bear_id, function (err, bear) {
+            if (err) {
+                res.send(err)
+            }
+
+            res.json(bear)
+        })
+    })
+
+
+// REGISTER OUR ROUTES
+// =============================================================================
+// All of our routes will be prefixed with /api
 app.use('/api', router)
 
 
