6bd99b8af205c11e79d2c8a2b0c4ccd75961ee20
Francis Rodrigues <dev@tosystems.net>
2017-10-26 14:37:51 -0200

    Deleting a Bear DELETE

--- server.js
+++ server.js
@@ -146,6 +146,19 @@ router.route('/bears/:bear_id')
         })
     })
 
+    // Delete the bear with the id (accessed at DELETE http://localhost:8080/api/bears/:bear_id)
+    .delete(function (req, res) {
+        Bear.remove({
+            _id: req.params.bear_id
+        }, function (err, bear) {
+            if (err) {
+                res.send(err)
+            }
+
+            res.json({message: 'Successfully deleted!'})
+        })
+    })
+
 
 //- Register our routes.
 // All of our routes will be prefixed with /api
